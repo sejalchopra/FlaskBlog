afc3b408be0a8871cf89bc0740f3f0edadc98751
Sejal Chopra <sejalchopra97@gmail.com>
2020-05-11 00:45:11 +0400

    Flaskapp

--- webapp/users/utils.py
+++ webapp/users/utils.py
@@ -1,4 +1,3 @@
-
 import os
 import secrets
 from PIL import Image
@@ -6,11 +5,12 @@ from flask import url_for, current_app
 from flask_mail import Message
 from webapp import mail
 
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
@@ -19,10 +19,11 @@ def save_picture(form_picture):
 
     return picture_fn
 
+
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender='sejalchopra97@gmail.com',
+                  sender='noreply@demo.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
